bb8f0301d9d89ab99f1fe5c6a5d827cf100d1681
Rostislav <suslovros@gmail.com>
2023-01-10 04:31:22 +0200

    add script for width and increased font-size social list items

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+const socialList = document.querySelector('.person-info__social');
+let socialListItem = document.querySelectorAll('.person-info__social-item');
+ 
+socialListItem.length == 4 ? socialList.style.width = 100+"%" : null;
